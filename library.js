4d3d99d9ecd7b2fb35427f96bf4fff2b0900f4b2
Kaoru <dev@kaoruk.com>
2013-09-13 14:26:10 -0500

    Add input fields and submit button

--- library.js
+++ library.js
@@ -1,25 +1,33 @@
 $(document).ready(function(){
 
 	var books = [
-		{
-			title: 'TITLE',
-			author: 'AUTHOR',
-			status: 'avaiable'
-		},
-		{
-		    title: 'DIFFTITLE',
-		    author: 'DIFFAUTHOR',
-		    status: 'DIFFavailable'
-	  	}
-  	];
+    {
+      title: 'TITLE',
+      author: 'AUTHOR',
+      status: 'avaiable'
+    },
+    {
+      title: 'DIFFTITLE',
+      author: 'DIFFAUTHOR',
+      status: 'DIFFavailable'
+     }
+    ];
 
-  	var libTempHtml = $("#templates").html();
-  	var libTemplate = _.template(libTempHtml);
-  	//var newLibHtml = libTemplate({ title: 'Aur', author: "ausdf", status: "asfd" });
+    var libTempHtml = $("#templates .book").html();
+    var libTemplate = _.template(libTempHtml);
+    //var newLibHtml = libTemplate({ title: 'Aur', author: "ausdf", status: "asfd" });
 
-  	for(var i = 0; i < books.length; i++){
-  		var newLibHtml = libTemplate({title: books[i].title, author: books[i].author, status: books[i].status});
-  		$("#library").append(newLibHtml);
-  	}
+    var renderLibrary = function(){
+      for(var i = 0; i < books.length; i++){
+        var newLibHtml = libTemplate({title: books[i].title, author: books[i].author, status: books[i].status});
+        $("#library").append(newLibHtml);
+      }
+    };
+
+    renderLibrary();
+
+    $("#subBut").click(function(){
+    	alert('hi');
+    })
 });
 
