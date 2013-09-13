6a2afdc7344c7ddeaf26d8d28d0a2972d7ff08da
Kaoru <dev@kaoruk.com>
2013-09-13 14:19:59 -0500

    Add library via js

--- library.js
+++ library.js
@@ -2,15 +2,24 @@ $(document).ready(function(){
 
 	var books = [
 		{
-			title: '[TITLE]',
-			author: '[AUTHOR]',
+			title: 'TITLE',
+			author: 'AUTHOR',
 			status: 'avaiable'
 		},
 		{
-		    title: '[DIFFTITLE]',
-		    author: '[DIFFAUTHOR]',
+		    title: 'DIFFTITLE',
+		    author: 'DIFFAUTHOR',
 		    status: 'DIFFavailable'
 	  	}
   	];
 
+  	var libTempHtml = $("#templates").html();
+  	var libTemplate = _.template(libTempHtml);
+  	//var newLibHtml = libTemplate({ title: 'Aur', author: "ausdf", status: "asfd" });
+
+  	for(var i = 0; i < books.length; i++){
+  		var newLibHtml = libTemplate({title: books[i].title, author: books[i].author, status: books[i].status});
+  		$("#library").append(newLibHtml);
+  	}
 });
+
